403af4758da14b58b616f0e9f94675d2c723247b
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2023-03-12 16:32:50 -0700

    Add ability to decrease point cloud resolution

--- Assets/Scripts/PointCloud.cs
+++ Assets/Scripts/PointCloud.cs
@@ -7,15 +7,37 @@ public class PointCloud : MonoBehaviour {
 
     [SerializeField] private Image pointIcon;
     [SerializeField] private Image pointCloudHolder;
-    private int scale = 17;
+    private int scale = 17; // Just for display purposes
+    private int resolutionDecreaseFactor = 1; // Lidar points will be clamped to every X units vertically and horizontally
 
     public void AddPoint(Vector3 hitPoint) {
         Image newPointIcon = Instantiate(pointIcon, pointCloudHolder.transform);
+        newPointIcon.gameObject.SetActive(true);
+        hitPoint.x = RoundToNearest(resolutionDecreaseFactor, hitPoint.x);
+        hitPoint.y = RoundToNearest(resolutionDecreaseFactor, hitPoint.y);
+        hitPoint.z = RoundToNearest(resolutionDecreaseFactor, hitPoint.z);
+
         newPointIcon.rectTransform.localPosition = new Vector3(hitPoint.x * scale, hitPoint.z * scale);
     }
 
     public void ShiftPointCloud(Vector3 walkerDisplacement) {
         pointCloudHolder.rectTransform.localPosition = new Vector3(walkerDisplacement.x * -scale, walkerDisplacement.z * -scale);
+    }
+
+    /// <summary>
+    /// Rounds given number to nearest interval. Returns higher number if the number is exactly between upper and lower bounds.
+    /// </summary>
+    /// <returns>Rounded number</returns>
+    private float RoundToNearest(float interval, float value) {
+        float lowerBound = value - (value % interval);
+        float upperBound = lowerBound + interval;
+        float midPoint = (upperBound - lowerBound) / 2;
+
+        if (value < midPoint) {
+            return lowerBound;
+        } else {
+            return upperBound;
+        }
     }
 
 }
