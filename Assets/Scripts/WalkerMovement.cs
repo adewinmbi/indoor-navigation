be5c209b203903c163021dff7731d1fec080f30a
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2023-03-12 14:59:24 -0700

    Add walker wasd movement

--- Assets/Scripts/WalkerMovement.cs
+++ Assets/Scripts/WalkerMovement.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class WalkerMovement : MonoBehaviour {
 
     [SerializeField] private PointCloud pointCloud;
+    [SerializeField] private Vector2 translationSpeed;
+    [SerializeField] private double rotationSpeed;
 
     private void Update() {
+        Vector2 translation = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * translationSpeed;
+        translation *= Time.deltaTime;
+        transform.Translate(new Vector3(translation.x, 0, translation.y));
+
         pointCloud.ShiftPointCloud(transform.position);
     }
 
