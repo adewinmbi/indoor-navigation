3029ab8141e0a93b38c4bd8fb12097c999fae70a
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2023-03-26 15:28:14 -0700

    Change walker movement to just arrow keys

--- Assets/Scripts/WalkerMovement.cs
+++ Assets/Scripts/WalkerMovement.cs
@@ -9,8 +9,24 @@ public class WalkerMovement : MonoBehaviour {
     [SerializeField] private double rotationSpeed;
 
     private void Update() {
-        Vector2 translation = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * translationSpeed;
-        translation *= Time.deltaTime;
+        Vector2 translation = new Vector2();
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            translation.y = 1;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            translation.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            translation.x = 1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            translation.x = -1;
+        }
+
+        translation *= Time.deltaTime * translationSpeed;
         transform.Translate(new Vector3(translation.x, 0, translation.y));
 
         pointCloud.UpdateWalkerPosition(transform.position);
