21fc0a5c4f9cf9c1043989e663b7614274823a15
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2023-04-04 08:19:43 -0700

    Update BLE beacons
    Used 3 beacons and made them directional to be more accurate to the real walker.

--- Assets/Scripts/BLEBeacon.cs
+++ Assets/Scripts/BLEBeacon.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BLEBeacon : MonoBehaviour {
 
-    [SerializeField] private GameObject walker;
     [SerializeField] private GameObject watch;
+    [SerializeField] private string beaconName;
+    [SerializeField] private Text displayText;
 
     // Update is called once per frame
     void Update() {
-        float dot = Vector3.Dot(walker.transform.forward, (watch.transform.position - walker.transform.position).normalized);
-        Debug.Log("Beacon: " + dot);
+        float dot = Vector3.Dot(transform.up, (watch.transform.position - transform.position).normalized);
+        float distance = Vector3.Distance(transform.position, watch.transform.position);
+        displayText.text = beaconName + ": " + dot;
     }
 }
